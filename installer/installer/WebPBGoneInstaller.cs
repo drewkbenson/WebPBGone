66b3c83a3f996961566850cbc5391744d5919915
Drew Benson <drewkbenson@yahoo.com>
2024-02-27 21:01:24 -0500

    Fixed installer, this thing writes to the registry now

--- installer/installer/WebPBGoneInstaller.cs
+++ installer/installer/WebPBGoneInstaller.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.Deployment.WindowsInstaller;
-using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
-using Microsoft.Win32;
-using System;
-using System.Drawing;
-using System.Windows.Forms;
+﻿using System;
 using WixSharp;
 
 namespace installer
@@ -16,14 +11,44 @@ namespace installer
             // The .reg file method (commented out) was adding an extra "\" before the "%" in the progfile
             //var fullSetup = new Feature("Registry Edits");
             //IEnumerable<RegValue> regValues = Tasks.ImportRegFile("add_registers.reg").ForEach(r => r.Feature = fullSetup);
+            //regValues.ToWObject()
 
+            /*
+
+            [HKEY_CLASSES_ROOT\.webp]
+            @= "WebPBGone"
+
+            [HKEY_CLASSES_ROOT\WebPBGone]
+            @= "WebP Image"
+            "Content Type" = "image/webp"
+            "PerceivedType" = "image"
+            "ImageOptionFlags" = dword:00000001
+
+            [HKEY_CLASSES_ROOT\WebPBGone\DefaultIcon]
+            @= "[ProgramFilesFolderX86]\\WebPBGone\\WebPBGone.exe,0"
+
+            [HKEY_CLASSES_ROOT\WebPBGone\shell]
+            @= "open"
+
+            [HKEY_CLASSES_ROOT\WebPBGone\shell\open]
+
+            [HKEY_CLASSES_ROOT\WebPBGone\shell\open\command]
+            @= "[ProgramFilesFolderX86]\\WebPBGone\\WebPBGone.exe \"%1\""
+
+            */
+
+            var setup = new Feature("WebPBGone");
             var project = new Project("WebPBGone",
                 new Dir(@"%ProgramFiles%\WebPBGone",
                     new DirFiles(@"..\..\src\bin\Release\*.dll"),
-                    new WixSharp.File(@"..\..\src\bin\Release\WebPBGone.exe")),
-                //regValues.ToWObject()
-                new ManagedAction(RegistryKeys.addRegistryKeys)
-                );
+                    new File(@"..\..\src\bin\Release\WebPBGone.exe")),
+                new RegValue(new Id("WebP"), setup, RegistryHive.ClassesRoot, @".webp", "", "WebPBGone"),
+                new RegValue(new Id("WebPBGone"), setup, RegistryHive.ClassesRoot, @"WebPBGone", "", "WebP Image"),
+                new RegValue(new Id("Icon"), setup, RegistryHive.ClassesRoot, @"WebPBGone\DefaultIcon", "", "C:\\Program Files (x86)\\WebPBGone\\WebPBGone.exe,0"),
+                new RegValue(new Id("Shell"), setup, RegistryHive.ClassesRoot, @"WebPBGone\shell", "", "open"),
+                new RegKey(setup, RegistryHive.ClassesRoot, @"WebPBGone\shell\open"),
+                new RegValue(new Id("Command"), setup, RegistryHive.ClassesRoot, @"WebPBGone\shell\open\command", "", "\"C:\\Program Files (x86)\\WebPBGone\\WebPBGone.exe\" %1")
+                ); ;
             project.LicenceFile = @".\License.rtf";
             project.InstallPrivileges = InstallPrivileges.elevated;
 
@@ -43,66 +68,6 @@ namespace installer
             projectLite.InstallPrivileges = InstallPrivileges.elevated;
 
             Compiler.BuildMsi(projectLite);
-        }
-    }
-
-    public class RegistryKeys
-    {
-        [CustomAction]
-        public static ActionResult addRegistryKeys(Session session)
-        {
-            //Registry.SetValue(@"HKEY_LOCAL_MACHINE\Software\Classes\.webp", null, "WebPBGone");
-            try
-            {
-                var x86Path = Environment.GetEnvironmentVariable("ProgramFiles(x86)");
-                var ProgFile = "\"" + x86Path + "\\WebPBGone\\WebPBGone.exe\" %1";
-
-                /*
-                Registry.SetValue(@"HKEY_CLASSES_ROOT\WebPBGone\shell\Convert to PNG\command", null, ProgFile);
-                Registry.SetValue(@"HKEY_CLASSES_ROOT\.webp\OpenWithProgids", @"WebPBGone", 0);
-                Registry.SetValue(@"HKEY_CLASSES_ROOT\.webp", null, @"WebPBGone");
-                Registry.SetValue(@"HKEY_CLASSES_ROOT\WebPBGone\Application", "ApplicationName", "WebPBGone");
-                */
-                Registry.SetValue(@"HKEY_CLASSES_ROOT\.webp", null, "WebPBGone");
-                Registry.SetValue(@"HKEY_CLASSES_ROOT\WebPBGone", null, "WebP Image");
-                Registry.SetValue(@"HKEY_CLASSES_ROOT\WebPBGone", "Content Type", "image/webp");
-                Registry.SetValue(@"HKEY_CLASSES_ROOT\WebPBGone", "PerceivedType", "image");
-                Registry.SetValue(@"HKEY_CLASSES_ROOT\WebPBGone", "ImageOptionFlags", 00000001, RegistryValueKind.DWord);
-                Registry.SetValue(@"HKEY_CLASSES_ROOT\WebPBGone\DefaultIcon", null, x86Path + "\\WebPBGone\\WebPBGone.exe,0");
-                Registry.SetValue(@"HKEY_CLASSES_ROOT\WebPBGone\shell", null, "open");
-                Registry.ClassesRoot.CreateSubKey(@"HKEY_CLASSES_ROOT\WebPBGone\shell\open");
-                var commandKey = Registry.ClassesRoot.CreateSubKey(@"HKEY_CLASSES_ROOT\WebPBGone\shell\open\command");
-                commandKey.SetValue("", x86Path + "\\WebPBGone\\WebPBGone.exe \"%1\"");
-
-                /*
-                
-                [HKEY_CLASSES_ROOT\.webp]
-                @= "WebPBGone"
-
-                [HKEY_CLASSES_ROOT\WebPBGone]
-                @= "WebP Image"
-                "Content Type" = "image/webp"
-                "PerceivedType" = "image"
-                "ImageOptionFlags" = dword:00000001
-
-                [HKEY_CLASSES_ROOT\WebPBGone\DefaultIcon]
-                @= "[ProgramFilesFolderX86]\\WebPBGone\\WebPBGone.exe,0"
-                
-                [HKEY_CLASSES_ROOT\WebPBGone\shell]
-                @= "open"
-                        
-                [HKEY_CLASSES_ROOT\WebPBGone\shell\open]
-
-                [HKEY_CLASSES_ROOT\WebPBGone\shell\open\command]
-                @= "[ProgramFilesFolderX86]\\WebPBGone\\WebPBGone.exe \"%1\""
-
-                */
-            }
-            catch
-            {
-                return ActionResult.Failure;
-            }
-            return ActionResult.Success;
         }
     }
 }
